345c3edf429d54fd4c99e0164c45d8dac3e20fba
Ivan Bašljan <ibasljan22@foi.hr>
2024-05-18 11:39:00 +0200

    Napravljena klasa Zahtjev.

---
+++ Software/MyDoc/MyDoc/Models/Zahtjev.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+
+namespace MyDoc.Models
+{
+    public class Zahtjev
+    {
+        public int Id { get; set; }
+
+        public string VrijemeTermina {  get; set; }
+        public string Obrazloženje { get; set; }
+        public bool MogućnostVideoPoziva { get; set; }
+        public string Status { get; set; }
+        public string Opis()
+        {
+            return Obrazloženje;
+        }
+    }
+}
